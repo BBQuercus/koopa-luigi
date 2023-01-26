971a481db0cdb2d9fec69e7cd275f79c67c78d94
Bastian Eichenberger <bastian@eichenbergers.ch>
2023-01-26 16:47:20 +0100

    Improve slurm resource requirements

--- slurm_cpu.sh
+++ slurm_cpu.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-#SBATCH --mail-user=
-#SBATCH --mail-type=END
 #SBATCH --account=fly-image
-#SBATCH --job-name="koopa-cpu" # Something that helps you recognize your job in the queue
+#SBATCH --cpus-per-task=48   # CPU cores per task
+#SBATCH --job-name="koopa-cpu"
+#SBATCH --mail-type=END
+#SBATCH --mail-user=
+#SBATCH --mem=128G  # set memory requirements
+#SBATCH --nodes=1  # ensure that all cores are on one machine
+#SBATCH --ntasks=1  # non MPI applications are usually single task
 #SBATCH --output=./logs/%x-%j.log
-#SBATCH --nodes=1 # ensure that all cores are on one machine
-#SBATCH --ntasks=1    # non MPI applications are usually single task, but you can do multiple tasks as well
-#SBATCH --time=12:00:00    # time required by the job, if you hit this limit the job will be terminated
-#SBATCH --cpus-per-task=28   # how many CPU cores per task do you need?
-#SBATCH --partition=cpu_short # for CPU only change this, e.g. to cpu_short, see also sinfo command output
+#SBATCH --partition=cpu_short  # partition (cpu_short for jobs <12:00:00)
+#SBATCH --time=12:00:00  # time required by the job
 
 # Configuration
 CONDA_DIR=/tungstenfs/scratch/gchao/eichbast/miniconda/bin/activate
@@ -17,4 +18,4 @@ CONFIG=/tungstenfs/scratch/gchao/eichbast/koopa-flows/tests/config/flies.cfg
 
 # Run
 source $CONDA_DIR koopa
-koopa-luigi --config $CONFIG --workers 28
+koopa-luigi --config $CONFIG --workers 8
--- slurm_gpu.sh
+++ slurm_gpu.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-#SBATCH --mail-user=
-#SBATCH --mail-type=END
 #SBATCH --account=fly-image
-#SBATCH --job-name="koopa-gpu" # Something that helps you recognize your job in the queue
+#SBATCH --cpus-per-task=16   # CPU cores per task
+#SBATCH --gres=gpu:a40:1  # what kind and how many GPUs do you need
+#SBATCH --job-name="koopa-gpu"
+#SBATCH --mail-type=END
+#SBATCH --mail-user=
+#SBATCH --mem=64G  # set memory requirements
+#SBATCH --nodes=1  # ensure that all cores are on one machine
+#SBATCH --ntasks=1  # non MPI applications are usually single task
 #SBATCH --output=./logs/%x-%j.log
-#SBATCH --nodes=1 # ensure that all cores are on one machine
-#SBATCH --ntasks=1    # non MPI applications are usually single task, but you can do multiple tasks as well
-#SBATCH --time=04:00:00    # time required by the job, if you hit this limit the job will be terminated
-#SBATCH --cpus-per-task=8   # how many CPU cores per task do you need?
-#SBATCH --partition=gpu_short # for CPU only change this, e.g. to cpu_short, see also sinfo command output
-#SBATCH --gres=gpu:a40:1  # what kind and how many GPUs do you need. We only have v100, max 4 per node
+#SBATCH --partition=gpu_short  # partition (gpu_short for jobs <12:00:00)
+#SBATCH --time=04:00:00  # time required by the job, if you hit this limit the job will be terminated
 
 # Configuration
 CONDA_DIR=/tungstenfs/scratch/gchao/eichbast/miniconda/bin/activate
